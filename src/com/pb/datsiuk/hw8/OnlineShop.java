be5272cd97d3ec7e84ae377c64bff446f69723f2
VictoriaDatsiuk <victoriadatsiuk@gmail.com>
2021-11-20 21:17:56 +0200

    init commit

---
+++ src/com/pb/datsiuk/hw8/OnlineShop.java
@@ -0,0 +1,14 @@
+package com.pb.datsiuk.hw8;
+
+import java.awt.event.ActionEvent;
+
+public class OnlineShop {
+            public static void main(String[] args) {
+            new Auth() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+
+                }
+            };
+    }
+}
